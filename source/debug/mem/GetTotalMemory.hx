8f26627d521a564aa4aeef541fa8e6ac315f9fed
ArkoseLabsOfficial <28umutmehmet28@gmail.com>
2025-12-28 22:54:26 +0000

    ...

--- source/debug/mem/GetTotalMemory.hx
+++ source/debug/mem/GetTotalMemory.hx
@@ -9,7 +9,7 @@ import cpp.SizeT;
  * Original C code by David Robert Nadeau
  * @see https://web.archive.org/web/20190716205300/http://nadeausoftware.com/articles/2012/07/c_c_tip_how_get_process_resident_set_size_physical_memory_use
  */
-@:buildXml('<include name="${this_dir}/build.xml" />')
+@:buildXml('<include name="${PRJ_DIR}/source/debug/mem/build.xml" />')
 @:include("memory.h")
 extern class GetTotalMemory
 {
