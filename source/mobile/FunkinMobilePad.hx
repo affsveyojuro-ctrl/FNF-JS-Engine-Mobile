538b3c6ee3a9d32f6545a4a27c566a995384027b
ArkoseLabsOfficial <28umutmehmet28@gmail.com>
2025-12-28 02:28:40 +0000

    add returnKey back

--- source/mobile/FunkinMobilePad.hx
+++ source/mobile/FunkinMobilePad.hx
@@ -41,7 +41,77 @@ class FunkinMobilePad extends MobilePad {
 		return button;
 	}
 
-	public function new(DPad:String, Action:String, globalAlpha:Float = 0.7) {
-		super(DPad, Action, globalAlpha);
+	public override function addButton(name:String, IDs:Array<String>, ?uniqueID:Int = -1, X:Float, Y:Float, Graphic:String, ?Scale:Float = 1.0, ?Color:Int = 0xFFFFFF, indexType:String = 'DPad', ?returnKey:String) {
+		var button:MobileButton = new MobileButton(0, 0);
+		button = createVirtualButton(X, Y, Graphic, Scale, Color);
+		button.name = name;
+		button.uniqueID = uniqueID;
+		button.IDs = IDs;
+		button.returnKey = returnKey;
+		button.onDown.callback = () -> onButtonDown.dispatch(button, IDs, uniqueID);
+		button.onOut.callback = button.onUp.callback = () -> onButtonUp.dispatch(button, IDs, uniqueID);
+
+		actions.push(button);
+		add(button);
+		buttonFromName.set(name, button);
+		switch (indexType.toUpperCase()) {
+			case 'DPAD':
+				buttonIndexFromName.set(name, countedDPadIndex);
+				countedDPadIndex++;
+			case 'ACTION':
+				buttonIndexFromName.set(name, countedActionIndex);
+				countedActionIndex++;
+		}
+	}
+
+	public function new(?DPad:String, ?Action:String, ?globalAlpha:Float = 0.7, ?disableCreation:Bool) {
+		if (!disableCreation)
+		{
+			if (DPad != "NONE")
+			{
+				if (!MobileConfig.dpadModes.exists(DPad))
+					throw 'The mobilePad dpadMode "$DPad" doesn\'t exists.';
+
+				for (buttonData in MobileConfig.dpadModes.get(DPad).buttons)
+				{
+					if (buttonData.scale == null) buttonData.scale = 1.0;
+					var buttonName:String = buttonData.button;
+					var buttonIDs:Array<String> = buttonData.buttonIDs;
+					var buttonUniqueID:Int = (buttonData.buttonUniqueID != null ? buttonData.buttonUniqueID : -1);
+					var buttonGraphic:String = buttonData.graphic;
+					var buttonScale:Float = buttonData.scale;
+					var buttonColor = buttonData.color;
+					var buttonX:Float = buttonData.x;
+					var buttonY:Float = buttonData.y;
+					var buttonReturn:Float = buttonData.returnKey;
+
+					addButton(buttonName, buttonIDs, buttonUniqueID, buttonX, buttonY, buttonGraphic, buttonScale, Util.colorFromString(buttonColor), 'DPad', buttonReturn);
+				}
+			}
+
+			if (Action != "NONE")
+			{
+				if (!MobileConfig.actionModes.exists(Action))
+					throw 'The mobilePad actionMode "$Action" doesn\'t exists.';
+
+				for (buttonData in MobileConfig.actionModes.get(Action).buttons)
+				{
+					if (buttonData.scale == null) buttonData.scale = 1.0;
+					var buttonName:String = buttonData.button;
+					var buttonIDs:Array<String> = buttonData.buttonIDs;
+					var buttonUniqueID:Int = (buttonData.buttonUniqueID != null ? buttonData.buttonUniqueID : -1);
+					var buttonGraphic:String = buttonData.graphic;
+					var buttonColor = buttonData.color;
+					var buttonScale:Float = buttonData.scale;
+					var buttonX:Float = buttonData.x;
+					var buttonY:Float = buttonData.y;
+					var buttonReturn:Float = buttonData.returnKey;
+
+					addButton(buttonName, buttonIDs, buttonUniqueID, buttonX, buttonY, buttonGraphic, buttonScale, Util.colorFromString(buttonColor), 'Action', buttonReturn);
+				}
+			}
+		}
+
+		super(DPad, Action, globalAlpha, true);
 	}
 }
