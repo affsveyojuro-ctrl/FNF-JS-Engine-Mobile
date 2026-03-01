67a64c6d152205c50557c1518d2a231d3e2601a2
affsveyojuro-ctrl <affsveyojuro@gmail.com>
2026-03-01 09:32:01 -0300

    Update Shaders.hx

--- source/Shaders.hx
+++ source/Shaders.hx
@@ -119,7 +119,7 @@ class Scanline extends FlxShader
 {
   @:glFragmentSource('
 		#pragma header
-		const float scale;
+		const float scale = 1.0;
 	uniform bool lockAlpha = false;
 		void main()
 		{
@@ -522,7 +522,7 @@ class VCRDistortionShader extends FlxShader // https://www.shadertoy.com/view/ld
       	vec2 look = uv;
         if(distortionOn){
         	float window = 1./(1.+20.*(look.y-mod(iTime/4.,1.))*(look.y-mod(iTime/4.,1.)));
-        	look.x = look.x + (sin(look.y*10. + iTime)/50.*onOff(4.,4.,.3)*(1.+cos(iTime*80.))*window)*(glitchModifier*2);
+        	look.x = look.x + (sin(look.y*10. + iTime)/50.*onOff(4.,4.,.3)*(1.+cos(iTime*80.))*window)*(glitchModifier*2.0);
         	float vShift = 0.4*onOff(2.,3.,.9)*(sin(iTime)*sin(iTime*20.) +
         										 (0.5 + 0.1*sin(iTime*200.)*cos(iTime)));
         	look.y = mod(look.y + vShift*glitchModifier, 1.);
@@ -603,11 +603,9 @@ class VCRDistortionShader extends FlxShader // https://www.shadertoy.com/view/ld
 
       gl_FragColor = mix(video,vec4(noise(uv * 75.)),.05);
 
-      if(curUV.x<0 || curUV.x>1 || curUV.y<0 || curUV.y>1){
-        gl_FragColor = vec4(0,0,0,0);
-      }
-
-    }
+    if(curUV.x < 0.0 || curUV.x > 1.0 || curUV.y < 0.0 || curUV.y > 1.0){
+      gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);
+   }
   ')
   public function new()
   {
